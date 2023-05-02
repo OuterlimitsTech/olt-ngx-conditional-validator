fbf9304330684c9ca50cadf3adccd1c6092ef07b
Chris Straw <chrisstraw@users.noreply.github.com>
2023-05-02 17:05:17 -0400

    Feature/14.0.0 (#1)
    * beta.0 release
    
    * wip
    
    * readme update
    
    * update versiopn

---
+++ projects/ngx-conditional-validator/src/lib/conditional-validator-builder.ts
@@ -0,0 +1,94 @@
+import { AsyncValidatorFn, ValidatorFn, Validators } from '@angular/forms';
+import { from, of } from 'rxjs';
+import { shareReplay, take } from 'rxjs/operators';
+import { ConditionalControl } from './conditional-validator';
+import { OltConditionalValidatorHelper } from './conditional-validator-helper';
+
+export class OltConditionalValidatorBuilder<T extends OltConditionalValidatorHelper> {
+
+    private unionConditon: Array<(helper: T) => boolean>;
+
+    constructor(private helper: T, conditons?: Array<(helper: T) => boolean>) {
+        this.unionConditon = conditons ?? [];
+    }
+
+    /** 
+     * Concatenate the condition
+     */
+    when(condition: (helper: T) => boolean) {
+        return new OltConditionalValidatorBuilder(this.helper, [...this.unionConditon, condition]);
+    }
+
+    /**
+     * Run validators when condition pass
+     */
+    then(validators: ValidatorFn | ValidatorFn[], otherwise?: { resetBy?: any }): ValidatorFn {
+        return this.run(validators, otherwise);
+    }
+
+    /**
+     * Enable control and run validators when condition pass, otherwise disable the control.
+     */
+    enable(validators?: ValidatorFn | ValidatorFn[]): ValidatorFn {
+        return this.run(validators, { disable: true });
+    }
+
+    private run(validators?: ValidatorFn | ValidatorFn[], otherwise?: { disable?: boolean, resetBy?: any }): ValidatorFn {
+        return (ctrl: ConditionalControl) => {
+            this.helper.control = ctrl;
+            const conditionPass = this.unionConditon.every(condition => condition(this.helper));
+
+            // hack
+            if (otherwise) {
+                if (conditionPass) {
+                    ctrl.conditionDisable = false;
+                    ctrl.conditionReset = undefined;
+                } else {
+                    ctrl.conditionDisable = otherwise.disable;
+                    ctrl.conditionReset = otherwise.resetBy;
+                }
+            }
+
+            if (conditionPass && !!validators) {
+                if (Array.isArray(validators)) {
+                    return Validators.compose(validators)!(ctrl);
+                } else {
+                    return validators(ctrl) || null;
+                }
+            } else {
+                return null;
+            }
+        };
+    }
+
+    /**
+     * Run async validators when condition pass.
+     * 
+     * Note: In current version, A control can only exist one.
+     */
+    thenAsync(validators: AsyncValidatorFn | AsyncValidatorFn[]): AsyncValidatorFn {
+        return (ctrl: ConditionalControl) => {
+            this.helper.control = ctrl;
+            const conditionPass = this.unionConditon.every(condition => condition(this.helper));
+
+            if (ctrl.lastPass === conditionPass && ctrl.lastValue === ctrl.value && ctrl.lastAsync) {
+                return ctrl.lastAsync;
+            }
+
+            ctrl.lastPass = conditionPass;
+            ctrl.lastValue = ctrl.value;
+
+            if (conditionPass) {
+                if (Array.isArray(validators)) {
+                    ctrl.lastAsync = from(Validators.composeAsync(validators)!(ctrl))
+                } else {
+                    ctrl.lastAsync = from(validators(ctrl));
+                }
+            } else {
+                ctrl.lastAsync = of(null);
+            }
+            ctrl.lastAsync = ctrl.lastAsync.pipe(shareReplay(1), take(1));
+            return ctrl.lastAsync;
+        };
+    }
+}
---
+++ projects/ngx-conditional-validator/src/lib/conditional-validator-helper.ts
@@ -0,0 +1,39 @@
+import { AbstractControl } from '@angular/forms';
+
+export class OltConditionalValidatorHelper {
+
+    control: AbstractControl | null = null;
+
+    get value() {
+        return this.control?.value;
+    }
+
+    /**
+     * Select the control with given path
+     * 
+     * It will try from current to root until getting a control
+     */
+    select(path: string) {
+        if (this.control) {
+            let result: AbstractControl | null = null;
+            let target: AbstractControl | null = this.control;
+
+            do {
+                result = target?.get(path) ?? null;
+                target = target?.parent ?? null;
+            } while (!result && target)
+
+            return result;
+        }
+        return null;
+    }
+
+    /**
+     * Select the control value with given path
+     * 
+     * note: It will try from current to root until getting a control
+     */
+    selectValue<T = any>(path: string): T {
+        return this.select(path)?.value;
+    }
+}
